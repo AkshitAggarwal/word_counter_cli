fa172f87ebe1b750efdf2726af0620b4f43eb393
Akshit Aggarwal <40717067+AkshitAggarwal@users.noreply.github.com>
2025-01-21 14:13:52 +0530

    Consolidate in a single method

--- main.py
+++ main.py
@@ -9,13 +9,31 @@ from typing_extensions import Annotated
 app = typer.Typer()
 
 
-@app.command("words")
-def word_count(file_name: str):
-    """Prints the number of words in a file."""
+def main(
+    file_name: str = None,
+    count_lines: Annotated[bool, typer.Option("--lines", "-l")] = False,
+    count_words: Annotated[bool, typer.Option("--words", "-w")] = False,
+    count_characters: Annotated[bool, typer.Option("--characters", "-c")] = False,
+):
+    """Main function for the script."""
+    output: str = ""
     with open(file_name, encoding="utf-8") as f:
-        words = f.read().split()
-    print(f"File {file_name} has {len(words)} words.")
+        if count_lines:
+            lines = f.readlines()
+            output = output + f"{len(lines)} lines"
+
+        if count_words:
+            words = f.read().split()
+            print(words)
+            output = output + f" {len(words)} words"
+
+        if count_characters:
+            characters = f.read()
+            print(characters)
+            output = output + f" {len(characters)} characters"
+
+    print(f"{output.strip()}.")
 
 
 if __name__ == "__main__":
-    app()
+    typer.run(main)
